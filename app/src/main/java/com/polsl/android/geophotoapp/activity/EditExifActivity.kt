a35f99ffb9cd3dfe78eceee6b87c31c891c15878
matelig <mateusz1994a@gmail.com>
2018-05-01 11:40:34 +0200

    get photo from clicked element on list and switch to edit exif activity

---
+++ app/src/main/java/com/polsl/android/geophotoapp/activity/EditExifActivity.kt
@@ -0,0 +1,13 @@
+package com.polsl.android.geophotoapp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.polsl.android.geophotoapp.R
+
+class EditExifActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_exif)
+    }
+}
